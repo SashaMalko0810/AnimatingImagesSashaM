75a5df68057851a9c561649d07aa807b005b9e20
Sasha Malko <sasha.malko@ocsbstudent.ca>
2018-03-07 10:54:03 -0500

    Hid the status bar

--- main.lua
+++ main.lua
@@ -3,3 +3,6 @@
 --Course: ICS2O/3C
 --Description: Thsi program displays 3 images moving in different
 -- directions, with some coloured text and a background
+
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
